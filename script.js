cddf1b7ab9d7ed2df89fea975b6cfb12447a6cf2
pranavkumar1999 <pranavprabhakarson@gmail.com>
2025-09-12 00:40:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// This file can be used for animations,
+// interactive elements, and other dynamic features.
+// For example, you can add code here to change the
+// navigation bar's background on scroll.
+
+// You can add more complex animations here later.
